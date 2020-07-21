0dfa4656d30737d42385a38cb748bb6beac1a341
Melissa Heredia <herediamelissa19@gmail.com>
2020-07-21 17:44:38 -0400

    Completed project

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MiCard extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -27,13 +28,51 @@ class MiCard extends StatelessWidget {
                     fontFamily: 'Pacifico'),
               ),
               Text(
-                'Android Developer',
+                'ANDROID DEVELOPER',
                 style: TextStyle(
                     color: Colors.teal[100],
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                     letterSpacing: 2.5,
                     fontFamily: 'Source Sans Pro'),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '248-825-1801',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'herediam@umich.edu',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'),
+                    )),
               ),
             ],
           ),
